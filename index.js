22e52553e4470bf3051220a2aa272268dd6368d2
Steve <stevesantner@gmail.com>
2022-10-13 20:40:32 -0400

    commit tree 137923700729b5359f6924b87ffa9b9dc8b81023

---
+++ index.js
@@ -0,0 +1,31 @@
+function introduction(name)
+{
+//a function to pass the samip aki test
+   if (name == "Aki") 
+{
+return "Hi, my name is Aki."
+}
+//just a lil seperation
+else if (name == "Samip")
+{
+    return "Hi, my name is Samip."
+}
+}
+
+function introductionWithLanguage(name, language)
+{
+
+    if (name == "Aki" && language == "Ember.js") 
+    { 
+
+    return "Hi, my name is Aki and I am learning to program in Ember.js."
+    }
+
+    else if (name == "Samip" && language == "React")
+{
+    return "Hi, my name is Samip and I am learning to program in React."
+}
+
+
+}
+
